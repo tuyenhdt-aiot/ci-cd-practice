c92fb8be7beb31c00fddd2d124081118608f26fe
tuyenhdt-aiot <tuyenhdt@aiot-global.com>
2025-06-23 00:05:18 +0700

    test: husky again

--- components/BadComponent.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const unused = 'I am unused'; 
-
-export const BadComponent = (props: any) => { 
-  console.log('Rendering BadComponent'); 
-
-  return <div>This is a bad component</div>;
-};
---
+++ src/components/BadComponent.tsx
@@ -0,0 +1,20 @@
+// import React from 'react';
+
+// const unused = 'I am unused';
+
+// export const BadComponent = (props: any) => {
+// console.log('Rendering BadComponent');
+
+// console.log('Hello');
+
+// return <div>This is a bad component</div>;
+// };
+import React from 'react';
+
+export const BadComponent = () => {
+  console.log('Rendering BadComponent');
+
+  console.log('Hello');
+
+  return <div>This is a bad component</div>;
+};
