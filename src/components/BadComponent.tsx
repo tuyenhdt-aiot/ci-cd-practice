5d869c40e7cc91e3f2dbedaba60a48a406a15353
Thanh Tuyen <tuyenhdt@aiot-global.com>
2025-06-23 09:44:08 +0700

    test: test husky again

--- src/components/BadComponent.tsx
+++ src/components/BadComponent.tsx
@@ -14,7 +14,7 @@ import React from 'react';
 export const BadComponent = () => {
 console.log('Rendering BadComponent');
 console.log('Hello');
-
+console.log('Hello');
   console.log('Hello');
 
   console.log('Test commit message');
